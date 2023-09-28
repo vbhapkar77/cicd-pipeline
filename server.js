e3cbb988db1f1e9c53380c61a9c2fbf8638b89c9
vbhapkar77 <vbhapkar77@gmail.com>
2023-09-28 13:03:42 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({
+        status: 200,
+        message: "Hello Vishal"
+    })
+})
+
+app.listen(8080, () => {
+    console.log('server running on port 8080')
+})
